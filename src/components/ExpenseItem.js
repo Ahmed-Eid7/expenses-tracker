bee3102eb22dfec2d3b1fab06d50958c94cf374f
Ahmed Eid <Ahmed_tech67@outlook.com>
2023-08-01 22:35:04 +0300

    Learning about create components, props and styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,18 @@
-function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+import ExpenseDate from "./ExpenseDate";
+
+import "./ExpenseItem.css";
+
+// You can destructuring it
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
