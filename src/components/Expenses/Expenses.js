ab3062e00e350b0a553372845289597e88c2e518
Ahmed Eid <Ahmed_tech67@outlook.com>
2023-08-04 22:16:59 +0300

    Learning about lifting data up and Events

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,10 +2,23 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -28,6 +41,6 @@ const Expenses = (props) => {
       />
     </Card>
   );
-}
+};
 
 export default Expenses;
